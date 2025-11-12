b1d76bf0b2cec6e4ac2d0773a838ca23f448318d
Marios Stefanidis <mstefanidis@knowledge.gr>
2025-11-12 16:57:29 +0200

    completed layout components

--- layout-components/src/RegularList.jsx
+++ layout-components/src/RegularList.jsx
@@ -2,10 +2,10 @@ export const RegularList = ({
   items,
   resourceName,
   itemComponent: ItemComponent,
-}) => (
+}) => {
   <>
-  {items.map((item, i) => (
-    <ItemComponent key={i} {...{ [resourceName]: item }} />
-  ))}
-  </>
-)
+    {items.map((item, i) => (
+      <ItemComponent key={i} {...{ [resourceName]: item }} />
+    ))}
+  </>;
+};
