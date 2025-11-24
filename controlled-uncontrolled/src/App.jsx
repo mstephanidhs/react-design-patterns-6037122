b247daceee77e3095243f73c034e70c4b3394c54
Marios Stefanidis <mstefanidis@knowledge.gr>
2025-11-24 11:50:11 +0200

    controlled/uncontrolled modal

--- controlled-uncontrolled/src/App.jsx
+++ controlled-uncontrolled/src/App.jsx
@@ -1,22 +1,26 @@
 import { useState } from 'react';
-import { ControlledForm } from "./ControlledForm"
-import { ControlledModal } from "./ControlledModal"
-import { UncontrolledForm } from "./UncontrolledForm"
+import { ControlledForm } from './ControlledForm';
+import { ControlledModal } from './ControlledModal';
+import { UncontrolledForm } from './UncontrolledForm';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
 
   return (
     <>
-      <ControlledModal shouldShow={showModal} onRequestClose={() => {}}>
+      <ControlledModal
+        shouldShow={showModal}
+        onRequestClose={() => setShowModal(false)}
+      >
         <ControlledForm
-          initialName="Shaun"
+          initialName='Shaun'
           initialAge={123}
-          initialHairColor="Brown" />
+          initialHairColor='Brown'
+        />
       </ControlledModal>
       <button onClick={() => setShowModal(true)}>Open Modal</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
