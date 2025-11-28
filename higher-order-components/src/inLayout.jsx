7e122eaca812225358ae989d4e45d3b7eb2bcf67
Marios Stefanidis <mstefanidis@knowledge.gr>
2025-11-28 12:27:44 +0200

    completed HOC section

---
+++ higher-order-components/src/inLayout.jsx
@@ -0,0 +1,11 @@
+export const inLayout = (Component) => (props) => {
+  return (
+    <div className='section'>
+      <div className='center-align'>
+        <div className='highlight'>
+          <Component {...props} />
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ higher-order-components/src/withUser.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+
+export const withUser = (Component, userId) => {
+  return (props) => {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+      setUser({
+        id: userId,
+        name: 'Shaun',
+        age: 123,
+        hairColor: 'Brown',
+        hobbies: ['code', 'food'],
+      });
+    });
+
+    if (!user) {
+      return <p>Loading...</p>;
+    }
+
+    return <Component {...props} user={user} />;
+  };
+};
